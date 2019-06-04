f32acba6bb2bf7b4ee8f9039cc45982123099ba4
Abdelrahman <abdelrahman@brown.edu>
2019-06-04 10:30:55 -0700

    fix live monitoring issue

--- src/runner/live_monitor.py
+++ src/runner/live_monitor.py
@@ -1,16 +1,18 @@
 import rethinkdb as r
 from django.conf import settings
+
+
 class LiveMonitor:
     def __init__(self, flow_id):
         self.flow_id = flow_id
         self.logs = ''
         self.conn = r.connect(settings.LIVE_MONITORING_URL, password=settings.LIVE_MONITORING_PASSWORD)
-        self.append(self.logs)
+        r.db('openroad').table('flow_log').insert({'openroad_uuid': flow_id,'logs': self.logs}).run(self.conn)
+
     
     def append(self, logs):
         self.logs += logs
-        r.db('openroad').table('flow_log').filter(r.row['openroad_uuid'] == self.flow_id).\
-			update({'logs': self.logs}).run(self.conn)
+        r.db('openroad').table('flow_log').filter(r.row['openroad_uuid'] == self.flow_id).update({'logs': self.logs}).run(self.conn)
 
     def close(self):
         self.conn.close()
