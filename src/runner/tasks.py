28dcaa73765b2523d9b0dbb0b1da86e67825cf84
Abdelrahman <abdelrahman@brown.edu>
2019-06-04 10:12:49 -0700

    fix import issue

--- src/runner/tasks.py
+++ src/runner/tasks.py
@@ -11,7 +11,7 @@ from celery.decorators import task
 from celery.utils.log import get_task_logger
 from git import Repo
 from storage import aws
-from live_monitor import LiveMonitor
+from .live_monitor import LiveMonitor
 
 logger = get_task_logger(__name__)
 
